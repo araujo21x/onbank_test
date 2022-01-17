e7b5d701e939b8bef47221411cdf34d0c3cca58f
Lucas de Araujo <araujolucas97@outlook.com>
2022-01-17 02:55:02 -0300

    auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { PassportModule } from '@nestjs/passport';
     PassportModule,
     JwtModule.register({
       secret: `${process.env.JWT_SECRET}`,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '604800s' },
     }),
   ],
   controllers: [AuthController],
--- src/custom-pokemon/custom-pokemon.controller.ts
+++ src/custom-pokemon/custom-pokemon.controller.ts
@@ -7,15 +7,18 @@ import {
   Param,
   Delete,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { CustomPokemonService } from './custom-pokemon.service';
 import { CreateCustomPokemonDto } from './dto/create-custom-pokemon.dto';
 import { UpdateCustomPokemonDto } from './dto/update-custom-pokemon.dto';
+import { JwtAuthGuard } from 'src/auth/shared/jwt-auth.guard';
 
 @Controller('custom-pokemon')
 export class CustomPokemonController {
   constructor(private readonly customPokemonService: CustomPokemonService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createCustomPokemonDto: CreateCustomPokemonDto) {
     return this.customPokemonService.create(createCustomPokemonDto);
@@ -31,6 +34,7 @@ export class CustomPokemonController {
     return this.customPokemonService.findOne(Number(id));
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(
     @Param('id') id: number,
@@ -39,6 +43,7 @@ export class CustomPokemonController {
     return this.customPokemonService.update(id, updateCustomPokemonDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.customPokemonService.remove(+id);
